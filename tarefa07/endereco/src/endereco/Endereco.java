fad1f623afddcdd91c88e389863db2e4411eedeb
mentejoao <skhulzxd@gmail.com>
2024-04-19 14:42:34 -0300

    criacao da classe Endereco

---
+++ tarefa07/endereco/src/endereco/Endereco.java
@@ -0,0 +1,11 @@
+package endereco;
+
+public class Endereco {
+
+    private Integer numero;
+    private String Complemento;
+    private Integer CEP;
+    private TipoLogradouro logradouro;
+    private Bairro bairro;
+    private TipoEndereco tipoEndereco;
+}
